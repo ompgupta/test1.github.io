160c0905b51f69257680b77b00e302d9eb7543ad
ompgupta <68551688+ompgupta@users.noreply.github.com>
2021-06-10 19:21:27 +0530

    twnb

---
+++ js/form.js
@@ -0,0 +1,6 @@
+const Qr_read =document.getElementById('Qr_read');
+const Form_fill =document.getElementById('form_fill');
+function nextFun(){
+  Form_fill.style.display='none';
+  Qr_read.style.display= 'block';
+}
